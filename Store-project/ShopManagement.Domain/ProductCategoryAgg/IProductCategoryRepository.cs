448deb05346a3abd266971cdfabb22778a739a4c
kamranmoghadam <k.moghadam2403@gmail.com>
2022-05-23 13:37:39 +0300

    ShopManagement.Application.Contracts &  SMApplication.& OperationResult & GenerateSlug &   IProductCategoryRepository  intialized

---
+++ Store-project/ShopManagement.Domain/ProductCategoryAgg/IProductCategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using ShopManagement.Application.Contracts.ProductCategory;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace ShopManagement.Domain.ProductCategoryAgg
+{
+    public interface IProductCategoryRepository
+    {
+        void Create(ProductCategory entity);
+        ProductCategory Get(long id);
+        List<ProductCategory> GetAll();
+
+        bool Exists(Expression<Func<ProductCategory, bool>>expression);
+        void SaveChenges();
+        EditProductCategory GetDetails(long id);
+        List<ProductCategoryViewModel> Search(ProductCategorySearchModel searchModel);
+        List<ProductCategoryViewModel> GetProductCategories();
+    }
+}
